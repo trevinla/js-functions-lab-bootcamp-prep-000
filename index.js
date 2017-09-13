fbd89193d78518d8ed979d2bed948bb8bd88e8d6
Trevor Seitz <trevinla@gmail.com>
2017-09-13 20:38:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function happyHolidaysTo(name) {
 }
 
 function holidayCountdown(holiday, days) {
-  return "It\'s ${days} days until ${holiday}!"
+  return "It\'s "+days+" days until "+holiday+"!"
 }

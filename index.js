82bbbf82e0f1d97f4f92c2aec6e60369dd574b41
Trevor Seitz <trevinla@gmail.com>
2017-09-13 20:34:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidayTo(name) {
-  return "Happy holidays ${name}!"
+function happyHolidaysTo(name) {
+  return "Happy holidays, ${name}!"
 }
 
 function holidayCountdown(holiday, days) {
